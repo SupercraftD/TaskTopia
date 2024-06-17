8bdacd072886bb918c9d12cde62ca47286ef3bd4
SupercraftD <dzhu700@gmail.com>
2024-06-17 11:56:06 -0400

    Connected Accounts to database and stores type now when accounts are created, a new document will be made in the database for them. This will store all the account info. For now, it only stores the account type. When logging into an account, if the account's type is different from currently logging in, it will tell you and won't let you.

--- login/login.js
+++ login/login.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
 
 
 const firebaseConfig = {
@@ -16,6 +17,7 @@ measurementId: "G-9SLQ2G0GEL"
 //initialize the firebase app and authentication
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
+const db = getFirestore(app)
 
 //initialize createNewAcc as false
 let createNewAcc = false
@@ -36,22 +38,31 @@ window.signup = function(type){
 //showInputs makes the buttons invisible and the credentials show up
 function showInputs(){
     document.getElementById("buttons").style.display = 'none';
-    document.getElementById("credentials").style.display = 'block';    
+    document.getElementById("credentials").style.display = 'grid';    
 }
 
 //submit function is called when user presses submit. Either logs in or signs up.
 window.submit = function(){
 
     //gets the email and password values from the inputs
-    let email = document.getElementById("email").value
-    let password = document.getElementById("password").value
+    let email = document.getElementById("emailInput").value
+    let password = document.getElementById("passwordInput").value
 
     //if createNewAcc is true, create a new account. Otherwise login.
     if (createNewAcc){
-        createUserWithEmailAndPassword(auth, email, password).then((userCredentials) =>{
+        createUserWithEmailAndPassword(auth, email, password).then(async(userCredentials) =>{
 
             //signed up successfully, send user to main page
             const user = userCredentials.user
+
+            console.log(user)
+            console.log(userCredentials)
+
+            await setDoc(doc(db, "Users", user.uid),{
+                type:window.accountType,
+                joined:serverTimestamp()
+            })
+
             window.location = "../index.html"
 
         }).catch((error)=>{
@@ -61,11 +72,28 @@ window.submit = function(){
 
         })
     }else{
-        signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+        signInWithEmailAndPassword(auth, email, password).then(async(userCredential) => {
 
             // Signed in successfully, send user to main page
             const user = userCredential.user;
-            window.location = "../index.html"
+
+            const userDoc = await getDoc(doc(db, "Users", user.uid))
+
+            if (userDoc.exists()){
+
+                const data = userDoc.data()
+
+                if (data.type != window.accountType){
+                    alert("attempting to sign into the wrong account type!")
+                    auth.signOut()
+                }else{
+                    window.location = "../index.html"
+                }
+
+            }else{
+                alert("error fetching account data")
+                auth.signOut()
+            }
             
 
         }).catch((error) => {
