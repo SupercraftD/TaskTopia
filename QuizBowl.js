dec3b05fc0e71112eaef67dac52a393632aa8555
MahitC <mahit.chekuri@gmail.com>
2024-06-19 12:12:12 -0400

    Create QuizBowl.js
    transition between lobby and question template page

---
+++ QuizBowl.js
@@ -0,0 +1,5 @@
+function startGame(){
+    document.getElementById("lobby").style.display = 'none';
+    document.getElementById("top").innerHTML="Question"
+    document.getElementById("game").style.display = 'grid';    
+}
